d95718fae4582123803649c872112c4e145be7d8
PenHsuanWang <willy118116@gmail.com>
2022-03-30 14:32:47 +0800

    remove kafka consumer from playground, re-gro under src

--- playground/online_machine_serving/python_kafka_consumer_serving.py
+++
@@ -1,9 +0,0 @@
-
-if __name__ == '__main__':
-    from kafka import KafkaConsumer
-
-    consumer = KafkaConsumer('testTopic', bootstrap_servers='localhost:9092')
-
-    while True:
-        for msg in consumer:
-            print(msg)
