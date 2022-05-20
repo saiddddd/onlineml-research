a521ffd63dd9d2c8bd2f2599131b649524f9e6e3
Pen-Hsuan Wang <willy118116@gmail.com>
2022-05-20 14:10:40 +0800

    batch run validation data process, testing data generator

--- src/streaming_data_source/data_inference_http_send.py
+++ src/streaming_data_source/data_inference_http_send.py
@@ -1,3 +1,5 @@
+import time
+
 import pandas as pd
 import requests
 import json
@@ -9,13 +11,36 @@ df = pd.read_csv('../../playground/quick_study/dummy_toy/dummy_data.csv')
 
 # df.pop('Y')
 
-df = df.head(50)
+select_index = 0
 
-df_to_json = df.to_json()
-wrap_to_send = {'Data': str(df_to_json)}
+while True:
+    sub_df = df.iloc[select_index:select_index+100]
+    print(len(sub_df.index))
+    select_index += 100
 
-# response = requests.post(api_url, data=str(df_to_json), headers=headers)
-response = requests.post(api_url, data=json.dumps(df_to_json), headers=headers)
+    if len(sub_df.index) > 0:
+        df_to_json = sub_df.to_json()
+        wrap_to_send = {'Data': str(df_to_json)}
 
-print(df_to_json)
-print(response.json())
+        # response = requests.post(api_url, data=str(df_to_json), headers=headers)
+        response = requests.post(api_url, data=json.dumps(df_to_json), headers=headers)
+
+        print(df_to_json)
+        print(response.json())
+        time.sleep(1)
+    else:
+        break
+
+
+
+# df = df.head(5)
+# print(df)
+#
+# df_to_json = df.to_json()
+# wrap_to_send = {'Data': str(df_to_json)}
+#
+# # response = requests.post(api_url, data=str(df_to_json), headers=headers)
+# response = requests.post(api_url, data=json.dumps(df_to_json), headers=headers)
+#
+# print(df_to_json)
+# print(response.json())
