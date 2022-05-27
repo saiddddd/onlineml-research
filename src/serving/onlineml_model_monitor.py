5721c906269aa37878f2a0d8113a093669984131
Pen-Hsuan Wang <willy118116@gmail.com>
2022-05-27 12:31:47 +0800

    design model inspection display in grid layout and add dbc external style sheets

--- src/serving/onlineml_model_monitor.py
+++ src/serving/onlineml_model_monitor.py
@@ -1,23 +1,104 @@
-
+import os, glob
 from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import base64
 
 from serving.onlineml_model_serving import OnlineMachineLearningModelServing
 
+external_stylesheets = [dbc.themes.BOOTSTRAP]
 
-def grid_layout_tree_structure_inspect_display():
-    image_filename = '../../output_plot/online_monitoring/current_tree_structure.png'  # replace with your own image
+# Static method to show figure from local file
+
+
+def tree_structure_inspect_display(display_fig_path: str):
+    if len(display_fig_path) > 0:
+        try:
+            image_filename = display_fig_path  # image path
+            encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+            return html.Div([
+                html.H3("current Hoeffding Tree Structure"),
+                html.Br(),
+                html.Img(
+                    src='data:image/png;base64,{}'.format(encoded_image.decode()),
+                    title='live check model structure'
+                )
+            ])
+        except:
+            return html.Div([
+                html.H3("Model Structure inspection figure path is not correct: {}, fail to load image!".format(image_filename)),
+            ])
+    else:
+        return html.Div([
+            html.H3("invalid input, please support correct input figure path: grid_layout_tree_structure_inspect_display(<path>)"),
+        ])
+
+
+def grid_layout_tree_structure_inspect_display(display_fig_dir: str):
+    if os.path.isdir(display_fig_dir):
+        listing_image = glob.glob(display_fig_dir+'*.png')
+        print(listing_image)
+
+    image_filename = listing_image[5]  # image path
     encoded_image = base64.b64encode(open(image_filename, 'rb').read())
-    return html.Div([
-        html.H3("current Hoeffding Tree Structure"),
-        html.Br(),
-        html.Img(
-            src='data:image/png;base64,{}'.format(encoded_image.decode()),
-            title='live check model structure'
+
+    card_content = [
+            dbc.CardImg(src='data:image/png;base64,{}'.format(encoded_image.decode()), top=True),
+            dbc.CardBody(
+                [
+                    html.H3("current Hoeffding Tree Structure"),
+                    # html.Br(),
+                    # html.Img(
+                    #     src='data:image/png;base64,{}'.format(encoded_image.decode()),
+                    #     title='live check model structure'
+                    # )
+                ]
+            ),
+
+        ]
+
+    # card = dbc.Card(
+    #     [
+    #         dbc.CardImg(src='data:image/png;base64,{}'.format(encoded_image.decode()), top=True),
+    #         dbc.CardBody(
+    #             [
+    #                 html.H3("current Hoeffding Tree Structure"),
+    #                 # html.Br(),
+    #                 # html.Img(
+    #                 #     src='data:image/png;base64,{}'.format(encoded_image.decode()),
+    #                 #     title='live check model structure'
+    #                 # )
+    #             ]
+    #         ),
+    #
+    #     ],
+    #     style={"width": "18rem"},
+    #     color="primary",
+    #     outline=True
+    # )
+
+    return html.Div(
+        dbc.Row(
+            [
+                dbc.Col(dbc.Card(card_content, color='primary', outline=True, style={"maxWidth": "540px"})),
+            ]
         )
-    ])
+    )
+
+    # return html.Div([
+    #             dbc.Row([
+    #                 dbc.Col([card]), dbc.Col([card]), dbc.Col([card]), dbc.Col([card]), dbc.Col([card])
+    #             ]),
+    #             dbc.Row([
+    #                 dbc.Col([card]), dbc.Col([card]), dbc.Col([card]), dbc.Col([card])
+    #             ]),
+    #             dbc.Row([
+    #                 dbc.Col([card]), dbc.Col([card])
+    #             ])
+    #         ])
+
+
 
 class ModelPerformanceMonitor:
 
@@ -32,7 +113,7 @@ class ModelPerformanceMonitor:
     def __init__(self):
 
         self._online_ml_server = OnlineMachineLearningModelServing.get_instance()
-        self.dash_display = Dash(__name__+'dash')
+        self.dash_display = Dash(__name__+'dash', external_stylesheets=external_stylesheets)
 
         # Multiple components can update everytime interval gets fired.
         @self.dash_display.callback(Output('live-update-acc-graph', 'figure'),
@@ -83,9 +164,13 @@ class ModelPerformanceMonitor:
                     ),
                 ])
             elif pathname == '/current_model_structure':
-                return grid_layout_tree_structure_inspect_display()
+                return tree_structure_inspect_display(
+                    display_fig_path='../../output_plot/online_monitoring/current_tree_structure.png')
 
-
+            elif pathname == '/test_grid_display':
+                return grid_layout_tree_structure_inspect_display(
+                    display_fig_dir='../../output_plot/online_monitoring/'
+                )
             else:
                 return html.Div([
                     dcc.Link(children='Go to Model Inference Performance Page.',
